c765a5d74ee4d00c06ffbc3e3c699c25e97d085c
DeMarcus Kennedy <demarcus.kennedy@ibm.com>
2021-06-18 17:54:33 -0500

    updated template
    Signed-off-by: DeMarcus Kennedy <demarcus.kennedy@ibm.com>

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
+
+// !NOTE routes go here - before app = express
+// !exp)
+// const user = require("./routes/user");
+// const userLog = require("./routes/userLog");
 
 var app = express();
 
@@ -12,6 +19,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+// !NOTE get/connet to db
+// !exp)
+// const db = require("./config/keys").mongoURI;
+// mongoose
+//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("connected to db"))
+//   .catch((err) => {
+//     "failed-" + console.log(err);
+//   });
+
+// !NOTE use routes
+// app.use("/api/user", user);
+// app.use("/api/user-log", userLog);
+
+// !NOTE This serves up client/build/index.html for all git requests
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
